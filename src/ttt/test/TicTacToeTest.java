e57ff1d51480d7485dbd9a7fe88e833f0f513bbe
JulesvanDongen <julesvandongen@gmail.com>
2016-03-07 22:00:21 +0100

    ttt tests toegevoegd

--- src/ttt/test/TicTacToeTest.java
+++ src/ttt/test/TicTacToeTest.java
@@ -16,17 +16,46 @@ public class TicTacToeTest {
     @Test
     public void testChooseMove() throws Exception {
         TicTacToe ttt = new TicTacToe();
-        assertTrue(ttt.chooseMove() == 5);
+
+        int[][] board = {
+                {TicTacToe.COMPUTER,    TicTacToe.HUMAN,    TicTacToe.EMPTY},
+                {TicTacToe.EMPTY,       TicTacToe.HUMAN,    TicTacToe.EMPTY},
+                {TicTacToe.EMPTY,       TicTacToe.EMPTY,    TicTacToe.EMPTY}
+        };
+
+        ttt.setBoard(board);
+
+        assertEquals(7, ttt.chooseMove());
     }
 
     @Test
     public void testPositionValue() throws Exception {
+        TicTacToe ttt = new TicTacToe();
 
+        int[][] board = {
+                {TicTacToe.EMPTY,       TicTacToe.HUMAN,    TicTacToe.EMPTY},
+                {TicTacToe.EMPTY,       TicTacToe.HUMAN,    TicTacToe.EMPTY},
+                {TicTacToe.EMPTY,       TicTacToe.HUMAN,    TicTacToe.EMPTY}
+        };
+
+        ttt.setBoard(board);
+
+        assertEquals(TicTacToe.HUMAN_WIN, ttt.positionValue());
     }
 
     @Test
     public void testAWin() throws Exception {
         TicTacToe ttt = new TicTacToe();
-//        ttt.
+
+        int[][] board = {
+                {TicTacToe.COMPUTER,    TicTacToe.COMPUTER, TicTacToe.COMPUTER},
+                {TicTacToe.EMPTY,       TicTacToe.EMPTY,    TicTacToe.EMPTY},
+                {TicTacToe.EMPTY,       TicTacToe.EMPTY,    TicTacToe.EMPTY}
+        };
+
+        ttt.setBoard(board);
+
+        assertTrue(ttt.isAWin(TicTacToe.COMPUTER));
+        assertFalse(ttt.isAWin(TicTacToe.HUMAN));
     }
 }
